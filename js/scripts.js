ded83c367c9d9c47c022c1a58d80ffe79b4b5010
Alvaro Flores and Bobby Crowston <alvaro@>
2016-08-16 09:58:03 -0700

    add final working counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(function(){
-  $("form").click(function(event){
-    for (var index = 0; index <= numInput; index+=1)
+  $("form").submit(function(event){
+    var numInput = parseInt($("#numInput").val());
+    var numCount = parseInt($("#numCount").val());
+
+    if (numCount > numInput){
+      alert("Please enter an amount less than your Count to number");
+    } else if (numCount < 1){
+      alert("Please enter a positive number");
+    } else if (!numCount || !numInput){
+      alert("Please enter a number");
+    }
+    for (var count = numCount; count <= numInput; count += numCount) {
+      $("#results").append("<li>" + count + "</li>");
+    }
+      event.preventDefault();
   });
 });
